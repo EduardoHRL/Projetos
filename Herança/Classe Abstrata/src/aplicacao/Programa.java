fccb63a5fbea6419a00884f6234f67c885f9bd8a
MalkemalD <eduardohrl18@gmail.com>
2024-05-23 21:01:45 -0300

    23/05/2024 - 21:01

---
+++ Herança/Classe Abstrata/src/aplicacao/Programa.java
@@ -0,0 +1,9 @@
+package aplicacao;
+
+import entidades.*;
+
+public class Programa {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Herança/Classe Abstrata/src/entidades/Conta.java
@@ -0,0 +1,7 @@
+package entidades;
+
+public abstract class Conta {
+    private Integer numero;
+    private 
+    
+}
